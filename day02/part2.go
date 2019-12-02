c09f07992523eee4f04cc75a79747c7b9511e96d
Wally Jones <wally@wallyjones.com>
2019-12-02 12:35:59 -0500

    Cleanup

--- day02/part2.go
+++ day02/part2.go
@@ -29,21 +29,28 @@ func main() {
 		fmt.Fprintln(os.Stderr, "reading standard input:", err)
 	}
 
+	// Map opcodes to functions
 	operations := map[int]func(int, int) int{
 		1: Add,
 		2: Multiply,
 	}
 
+	// Maintain an original slice of opcodes
 	origOps := make([]int, len(ops))
 	copy(origOps, ops)
 
+	// Try all possible comibinations of values from 0 to 99 in positions 1
+	// and 2
 	for j := 0; j < 100; j++ {
 		for k := 0; k < 100; k++ {
+			// Reset opcodes
 			copy(ops, origOps)
+
 			ops[1], ops[2] = j, k
 			for i := 0; ; i = i + 4 {
 				op := ops[i]
 
+				// Terrible overflow checking
 				if op == 99 || op > 2 || ops[i+1] > len(ops) || ops[i+2] > len(ops) || ops[i+3] > len(ops) {
 					break
 				}
@@ -59,8 +66,7 @@ func main() {
 				val := operations[op](x, y)
 
 				if val == 19690720 {
-					fmt.Println("found")
-					fmt.Println(ops[1], ops[2])
+					fmt.Println(100*ops[1] + ops[2])
 					return
 				}
 
