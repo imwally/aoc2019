ca6b50cab615a1d617715cc8fd5604c39976bdf6
Wally Jones <wally@wallyjones.com>
2019-12-02 12:31:26 -0500

    Clean up

--- day02/part1.go
+++ day02/part1.go
@@ -29,27 +29,31 @@ func main() {
 		fmt.Fprintln(os.Stderr, "reading standard input:", err)
 	}
 
-	operations := map[int]func(int, int) int{
+	// Map opcodes to functions
+	opCodes := map[int]func(int, int) int{
 		1: Add,
 		2: Multiply,
 	}
 
+	// Replace values before running
+	ops[1] = 12
+	ops[2] = 2
+
+	// Run the machine
 	for i := 0; ; i = i + 4 {
 		op := ops[i]
+
+		if op == 99 {
+			break
+		}
 
 		x := ops[ops[i+1]]
 		y := ops[ops[i+2]]
 		pos := ops[ops[i+3]]
 
-		fmt.Println(op, x, y, pos)
-		if op == 99 {
-			break
-		}
-		val := operations[op](x, y)
-
-		ops[pos] = val
-
-		fmt.Println(ops)
+		ops[pos] = opCodes[op](x, y)
 	}
 
+	// For some reason my result is in position 1
+	fmt.Println(ops[1])
 }
