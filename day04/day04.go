18c85e8a5a7b9f3725eadb6a2706e1273b240ad5
Wally Jones <wally@wallyjones.com>
2019-12-04 12:06:15 -0500

    Finish day 4

--- day04/day04.go
+++ day04/day04.go
@@ -27,30 +27,46 @@ func Increasing(s string) bool {
 	return true
 }
 
-func MoreThanDup(s string) bool {
+func LargerGroup(s string) bool {
 	dups := make(map[byte]int)
 	for i := 0; i < len(s); i++ {
 		dups[s[i]]++
 	}
 
-	for _, dup := range dups {
-		if dup > max {
-			return true
+	for _, v := range dups {
+		if v >= 2 {
+			for _, v2 := range dups {
+				if v2 == 2 {
+					return false
+				}
+			}
 		}
 	}
 
-	return false
+	return true
 }
 
 func main() {
 	start := 197487
 	end := 673251
 
+	// PART 1
+	var part1 []string
 	for i := start; i < end; i++ {
 		num := strconv.Itoa(i)
-		if Duplicates(num) && Increasing(num) && !MoreThanDup(num) {
-			fmt.Println(num)
+		if Duplicates(num) && Increasing(num) {
+			part1 = append(part1, num)
 		}
 	}
+	fmt.Println("Part 1:", len(part1))
 
+	// PART 2
+	var part2 []string
+	for i := start; i < end; i++ {
+		num := strconv.Itoa(i)
+		if Duplicates(num) && Increasing(num) && !LargerGroup(num) {
+			part2 = append(part2, num)
+		}
+	}
+	fmt.Println("Part 2:", len(part2))
 }
