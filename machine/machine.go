fa4d2b7a74fc0c3af4fa75dc0099d6955191f91f
Wally Jones <wally@wallyjones.com>
2019-12-05 23:00:24 -0500

    This is aboslutely fucking rubbish. I hate computers.

--- machine/machine.go
+++ machine/machine.go
@@ -3,7 +3,7 @@ package machine
 import "fmt"
 
 func Run(ops []int) []int {
-	for i := 0; ; {
+	for i := 0; ; i++ {
 		ins := ops[i]
 		op := ins % 100
 
@@ -11,59 +11,68 @@ func Run(ops []int) []int {
 			break
 		}
 
-		p1, p2, p3 := 0, 0, 0
-		if ins > 1000 {
-			params := ins - op
+		p1, p2 := 0, 0
+		params := ins - op
 
-			pm1 := params % 100
-			pm2 := params % 10000 / 1000
-			pm3 := params % 100000 / 10000
+		pm1 := params % 1000 / 100
+		pm2 := params % 10000 / 1000
 
-			i++
-			if pm1 == 0 {
-				p1 = ops[ops[i]]
-			} else {
-				p1 = ops[i]
-			}
+		i++
+		if pm1 == 0 {
+			p1 = ops[ops[i]]
+		} else {
+			p1 = ops[i]
+		}
 
+		if op < 3 || op == 5 || op == 6 || op == 7 || op == 8 {
 			i++
 			if pm2 == 0 {
 				p2 = ops[ops[i]]
 			} else {
 				p2 = ops[i]
 			}
+		}
 
-			i++
-			if pm3 == 0 {
-				p3 = ops[i]
-			} else {
-				p3 = i
-			}
-			i++
-		} else {
-			i++
-			p1 = ops[ops[i]]
-			i++
-			p2 = ops[ops[i]]
-			i++
-			p3 = ops[i]
+		if op < 3 || op == 7 || op == 8 {
 			i++
 		}
 
 		switch op {
 		case 1:
-			ops[p3] = p1 + p2
+			ops[ops[i]] = p1 + p2
 		case 2:
-			ops[p3] = p1 * p2
+			ops[ops[i]] = p1 * p2
 		case 3:
 			var v int
 			_, err := fmt.Scanf("%d", &v)
 			if err != nil {
 				panic("shit broke")
 			}
-			ops[p3] = v
+			ops[ops[i]] = v
 		case 4:
 			fmt.Println(p1)
+		case 5:
+			if p1 != 0 {
+				i = p2 - 1
+			}
+		case 6:
+			if p1 == 0 {
+				i = p2 - 1
+			}
+		case 7:
+			if p1 < p2 {
+				ops[ops[i]] = 1
+			} else {
+				ops[ops[i]] = 0
+			}
+		case 8:
+			if p1 == p2 {
+				ops[ops[i]] = 1
+			} else {
+				ops[ops[i]] = 0
+			}
+		default:
+			panic("unrecognized opcode")
 		}
 
 	}
