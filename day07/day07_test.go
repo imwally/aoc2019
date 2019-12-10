4f241a8fdd7ebc5cb0ce2fcad4d41b165569390a
Wally Jones <wally@wallyjones.com>
2019-12-10 01:31:35 -0500

    More work on day 7

--- day07/day07_test.go
+++ day07/day07_test.go
@@ -1,13 +1,13 @@
 package main
 
 import (
+	"fmt"
 	"testing"
 
 	"github.com/imwally/aoc2019/machine"
 )
 
 func TestPart1(t *testing.T) {
-
 	// Test 1
 	program := []int{3, 15, 3, 16, 1002, 16, 10, 16, 1, 16, 15, 15, 4, 15, 99, 0, 0}
 	seq := []int{4, 0, 3, 0, 2, 0, 1, 0, 0, 0, 0, 0}
@@ -58,4 +58,30 @@ func TestPart1(t *testing.T) {
 		t.Errorf("error: got %v, expected %v", got, expected)
 	}
 
+}
+
+func TestPart2(t *testing.T) {
+
+	program := [][]int{
+		[]int{3, 26, 1001, 26, -4, 26, 3, 27, 1002, 27, 2, 27, 1,
+			27, 26, 27, 4, 27, 1001, 28, -1, 28, 1005, 28, 6, 99, 0, 0, 5},
+		[]int{3, 26, 1001, 26, -4, 26, 3, 27, 1002, 27, 2, 27, 1,
+			27, 26, 27, 4, 27, 1001, 28, -1, 28, 1005, 28, 6, 99, 0, 0, 5},
+		[]int{3, 26, 1001, 26, -4, 26, 3, 27, 1002, 27, 2, 27, 1,
+			27, 26, 27, 4, 27, 1001, 28, -1, 28, 1005, 28, 6, 99, 0, 0, 5},
+		[]int{3, 26, 1001, 26, -4, 26, 3, 27, 1002, 27, 2, 27, 1,
+			27, 26, 27, 4, 27, 1001, 28, -1, 28, 1005, 28, 6, 99, 0, 0, 5},
+		[]int{3, 26, 1001, 26, -4, 26, 3, 27, 1002, 27, 2, 27, 1,
+			27, 26, 27, 4, 27, 1001, 28, -1, 28, 1005, 28, 6, 99, 0, 0, 5},
+	}
+
+	seq := []int{5, 6, 7, 8, 9}
+	output := make([]int, 10)
+
+	for i := 0; i < len(seq); i++ {
+		program[i], output = machine.Run(program[i], []int{seq[i], output[0], 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0})
+	}
+	program[0], output = machine.Run(program[0], []int{seq[0], output[0], 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0})
+	fmt.Println(0, program[0])
+	fmt.Println(output)
 }
